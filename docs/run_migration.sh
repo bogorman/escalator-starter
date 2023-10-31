035fcb475c5f520e73a878b71603a90333da9045
Barry O Gorman <bogorman@gmail.com>
2023-10-31 20:47:25 +0100

    Fix db url

--- docs/run_migration.sh
+++ docs/run_migration.sh
@@ -1,3 +1,3 @@
 export FLYWAY_PASSWORD=<CHANGE THIS>
-export FLYWAY_URL="jdbc:postgresql://localhost:5432/swotlegal?user=postgres" 
+export FLYWAY_URL="jdbc:postgresql://localhost:5432/escalatorstarter?user=postgres" 
 flyway -locations="filesystem:./" migrate
